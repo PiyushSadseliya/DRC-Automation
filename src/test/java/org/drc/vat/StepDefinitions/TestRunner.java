07bb1956ea7ad04161cd73d3b76b6a5a0b98dd66
Dhanshri Bhavsar <dhanshri.bhavsar@1rivet.local>
2018-06-21 12:52:10 +0530

    Completed

---
+++ src/test/java/org/drc/vat/StepDefinitions/TestRunner.java
@@ -0,0 +1,78 @@
+package org.drc.vat.StepDefinitions;
+
+import com.cucumber.listener.ExtentProperties;
+import com.cucumber.listener.Reporter;
+import cucumber.api.CucumberOptions;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.apache.commons.io.FileUtils;
+import org.drc.vat.appmanager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static org.drc.vat.appmanager.HelperBase.logout;
+
+@CucumberOptions(features = {"classpath:features/ReviewAndSubmit.feature"},
+       glue = "org.drc.vat.StepDefinitions",
+        plugin = {"com.cucumber.listener.ExtentCucumberFormatter:",
+                  "html:test-output/cucumber-report"})
+
+public class TestRunner extends AbstractTestNGCucumberTests {
+
+
+    private Logger logger = LoggerFactory.getLogger(TestRunner.class);
+    private String outputDir = "test-output/" + new Date().toString().substring(0,10);
+    private String timestamp = new SimpleDateFormat("_HHmmss").format(new Date());
+
+    private static ApplicationManager app =
+            new ApplicationManager((System.getProperty("browser", BrowserType.CHROME)));
+
+    @BeforeSuite
+    public void setUP_Mobilenop() throws IOException, AWTException
+    {
+        app.initUrl();
+        ExtentProperties extentProperties = ExtentProperties.INSTANCE;
+        extentProperties.setReportPath(outputDir + "/ExtentReport" + timestamp + ".html");
+    }
+
+    @AfterSuite(alwaysRun = true)
+    public void tearDown() throws IOException {
+        
+        app.stop();
+        Reporter.loadXMLConfig("src/test/resources/extent-config.xml");
+        Reporter.setSystemInfo("user", System.getProperty("user.name"));
+    }
+
+    @Before
+    public void startScenario(Scenario scenario) throws IOException 
+    {
+    	app.callurl();
+    	logger.info("Start scenario: " + scenario.getName());
+    }
+
+    @After
+    public void endScenario(Scenario scenario) throws Exception {
+    	logout();
+        String screenshot = scenario.getName();
+        File src = app.takeScreenshotAsFile();
+        File dest = new File(System.getProperty("user.dir") + "/" + outputDir + "/screenshots/" + screenshot + timestamp + ".png");
+        FileUtils.copyFile(src, dest);
+        Reporter.addScreenCaptureFromPath(dest.toString());
+
+        scenario.embed(app.takeScreenshot(), "image/png");
+        logger.info("Stop scenario: " + scenario.getName());
+    }
+}
+
+
---
+++ src/test/java/org/drc/vat/appmanager/ApplicationManager.java
@@ -0,0 +1,107 @@
+package org.drc.vat.appmanager;
+
+import java.awt.AWTException;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.net.URL;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.BrowserType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class ApplicationManager {
+    private final Properties properties;
+    public WebDriver wd;
+    private String browser;
+    
+    private HelperBase helperBase;
+
+    public ApplicationManager(String browser) {
+        this.browser = browser;
+        properties = new Properties();
+    }
+
+    public void initUrl() throws IOException, AWTException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(
+                String.format("src/test/resources/%s.properties", target))));
+        if ("".equals(properties.getProperty("selenium.server"))) {
+            if (browser.equals(BrowserType.IE)) {
+                //System.setProperty("webdriver.ie.driver","D:\\AutomationFHLBNY\\AuditConfirmations\\IEDriverServer.exe");
+                wd = new InternetExplorerDriver();
+
+            } else if (browser.equals(BrowserType.CHROME)) {
+                 // System.setProperty("webdriver.CHROME.driver","D:\\AutomationFHLBNY\\AuditConfirmations\\chromedriver.exe");
+                wd = new ChromeDriver();
+            }
+        } else {
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setBrowserName(browser);
+            wd = new RemoteWebDriver(new URL(properties.getProperty("selenium.server")), capabilities);
+            System.out.println(wd);
+        }
+
+        wd.manage().window().maximize();
+        wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        //wd.get(properties.getProperty("web.Url"));
+        //auth();
+
+        helperBase = new HelperBase(wd);
+    }
+
+    public void stop() {
+        wd.quit();
+        wd = null;
+    }
+
+    public HelperBase helperBase() {
+        return helperBase;
+    }
+    
+    public void callurl()
+    {
+    	wd.get(properties.getProperty("web.Url"));
+    }
+    
+    public byte[] takeScreenshot() {
+        return ((TakesScreenshot) wd).getScreenshotAs(OutputType.BYTES);
+    }
+
+    public File takeScreenshotAsFile() {
+        return ((TakesScreenshot) wd).getScreenshotAs(OutputType.FILE);
+    }
+
+/*
+    private void auth() throws AWTException {
+        String decoded = new String(DatatypeConverter.parseBase64Binary(properties.getProperty("pw")));
+        Robot robot = new Robot();
+        robotType(robot, properties.getProperty("un"));
+        robot.keyPress(KeyEvent.VK_TAB);
+        robotType(robot, decoded);
+        robot.keyPress(KeyEvent.VK_ENTER);
+    }
+
+    private void robotType(Robot robot, String characters) {
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection stringSelection = new StringSelection(characters);
+        clipboard.setContents(stringSelection, stringSelection);
+
+        robot.delay(500);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.delay(500);
+    }
+*/
+}
+
+
