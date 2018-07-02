b08745af6a25317a4cbd0885c6b8ee73fa719be6
frankey <frankeymehta@gmail.com>
2018-07-02 16:36:34 +0530

    Manage VAt Updated

--- src/test/java/org/drc/vat/StepDefinitions/ManageVatRegistration.java
+++ src/test/java/org/drc/vat/StepDefinitions/ManageVatRegistration.java
@@ -38,49 +38,60 @@ public class ManageVatRegistration
 	public void user_selects_Manage_Vat_Registraion() throws Throwable 
 	{
 		System.out.println("User selects Manage Vat Registraion ");
-		//locatour to find
-		//locatour comes here 
+		clickOn("ManageVAT","");
+		
 	}
 
 	@Then("^user selects value from filter drop down \"([^\"]*)\"$")
 	public void user_selects_value_from_filter_drop_down(String value) throws Throwable 
 	{
 		System.out.println("User select Filter ");
+		clickOn("FilterBy", "");
 		clickOn("span","[contains(text(),'" + value + "')]");
+		Thread.sleep(2000);
+		
 	}
-
+	//TC_04
+	@Then("^user selects All from filter dropdown$")
+	public void user_selects_All_from_filter_dropdown() throws Throwable 
+	{
+		System.out.println("All is selected");
+	}
+		
 	@Then("^enters data in type here text box \"([^\"]*)\"$")
 	public void enters_data_in_type_here_text_box(String value) throws Throwable 
 	{
 		System.out.println("User Enter Data to Filter ");
+		Thread.sleep(5000);
 		type("TypeHere",value);
+		Thread.sleep(5000);				
 	}
-	
-	
+
 	@When("^User selects all in filter$")
 	public void user_selects_all_in_filter() throws Throwable 
 	{
 		System.out.println("User Select all Filter ");
 		clickOn("FilterBy", "");
 	}
-
-	
-
+		
 	@Then("^clicks on download pdf button$")
 	public void clicks_on_download_pdf_button() throws Throwable 
 	{
-		
 		clickOn("DownloadPDF", "");
+		Thread.sleep(1000);
+		clickOn("DownloadPDFClick", "");		
 		verifyDownload("ManageVatRegistration");
 	}
-	
+
 	@Then("^user click on view$")
 	public void user_click_on_view() throws Throwable 
 	{
 		System.out.println("User Select View ");
-		type("ViewdownArrow","");
+		clickOn("ManageDropdown","");
+		clickOn("ViewdownArrow","");
+		Thread.sleep(5000);
 	}
-	
+
 	// Display Tax Payer Portal Title 
 	@Then("^it Navigate to Tax Payer Details$")
 	public void it_Navigate_to_Tax_Payer_Details() throws Throwable 
@@ -88,86 +99,113 @@ public class ManageVatRegistration
 		System.out.println("Tax Payer Portal Display ");
 		bodymessage("Tax Payer Details");
 	}
-	
+
 	// click on rejected
 	@And("^User click on Rejected$")
 	public void user_click_on_Rejected() throws Throwable 
 	{
 		clickOn("Rejected", "");
+		Thread.sleep(2000);		
 	}
 
 	// Rejected pop up 
 	@Then("^User See pop up Reason for Rejected$")
 	public void user_See_pop_up_Reason_for_Rejected() throws Throwable 
 	{
-		bodymessage("Reason for Rejection");
+		//String message = wd.findElement(By.xpath("//span[@class='col p-0']")).getText();
+		//String expectedMessage = "Reason for Rejection";
+		//Assert.assertTrue(message.contains(expectedMessage));
+		//Thread.sleep(2000);
+		
+		WebElement errorMessage= wd.findElement(By.xpath("//span[@class='col p-0']"));
+		Assert.assertEquals(errorMessage.getText(),"Reason for Rejection");
+		Thread.sleep(2000);		
 	}
-	
-	// select  proiority list 
+
+	//@StatusReview
+	@And("^user click on manage setting and select Review$")
+	public void user_click_on_manage_setting_and_select_Review() throws Throwable 
+	{
+		System.out.println("User Select View ");
+		clickOn("StatusReview","");
+		//Thread.sleep(2000);
+		clickOn("ReviewSelect","");
+		Thread.sleep(5000);
+	}	
+	//@StatusChangePriority  
 	@And("^user click on Change Priority$")
 	public void user_click_on_Change_Priority() throws Throwable 
 	{
-		//Locatours to find as it not showing
-		//clickOn("ViewdownArrow", "");
-		
+		clickOn("StatusChangePriority","");
+		Thread.sleep(2000);
+		clickOn("StatusChangePrioritySelect", "");
+		Thread.sleep(2000);
 	}
-
-	@And("^select priority from priority list$")
-	public void select_priority_from_priority_list() throws Throwable 
+	//@StatusChangePriority	
+	@And("^user selects priority from priority list \"([^\"]*)\"$")
+	public void user_selects_priority_from_priority_list(String value) throws Throwable 
 	{
-		// Locatours to find as it not showing 
-		System.out.println("Select Priority from list ");
-	}
-	
-	
+		clickOn("ChangePriorityDropDown", "");
+		clickOn(value, "");
+	}	
+	//@StatusChangePriority	
 	@Then("^click on right tick$")
 	public void click_on_right_tick() throws Throwable 
 	{
-		// Locatours to find as it not showing
 		System.out.println("Click on right tick ");
-	}
+		clickOn("TickRight","");
+	}	
 	
-	
-	
-	@Then("^Priority will change$")
-	public void priority_will_change() throws Throwable 
+	//@StatusChangePriority
+	@Then("^Priority is Changed and toast message display$")
+	public void priority_is_Changed_and_toast_message_display() throws Throwable 
 	{
+		Thread.sleep(2000);
+		String expectedMessage = "Priority update successfully!";
+		String message = wd.findElement(By.xpath("//*[@class='toast-top-right toast-container']")).getText();
+		Assert.assertTrue(message.contains(expectedMessage));
 		System.out.println("Priority changed ");
 	}
-	
-	
+
+	//@StatusReassign
 	@And("^user click on Reassign$")
 	public void user_click_on_Reassign() throws Throwable 
 	{
-		//have to check as it changing
-		//clickOn("ViewdownArrow", "");
+		clickOn("ManageDropdown","");
+		clickOn("StatusReassign","");						
 	}
-	
-	@And("^select another Tax Officier fron the list$")
-	public void select_another_Tax_Officier_fron_the_list() throws Throwable 
+
+	@Then("^select another Tax Officier fron the list \"([^\"]*)\"$")
+	public void select_another_Tax_Officier_fron_the_list(String value) throws Throwable 
 	{
-	   //locators to find 
 		System.out.println("another officer selected  ");
+		clickOn("SelectDropdownTaxOfficer", ""); 
+		clickOn(value, "");
 	}
-	
+
+	@And("^click on right tick for reassign$")
+	public void click_on_right_tick_for_reassign() throws Throwable 
+	{
+		clickOn("TickRight","");
+	}
 
 	@Then("^Tax Officier Changed and toast message display$")
 	public void tax_Officier_Changed_and_toast_message_display() throws Throwable 
 	{
-		// Locotors to find
+		Thread.sleep(2000);
+		String expectedMessage = "Tax officer reassign successfully!";
+		String message = wd.findElement(By.xpath("//*[@class='toast-top-right toast-container']")).getText();
+		Assert.assertTrue(message.contains(expectedMessage));
 		System.out.println("Officier Changed  ");
-		// toast display 
-		toastMessage();
-		//wait("Success Tax officer reassign successfully!");
 	}
-	
+
 	//on hold
 	@And("^user select show \"([^\"]*)\"$")
 	public void user_select_show(String arg1) throws Throwable 
 	{
 		System.out.println("Show Changed  ");
 	}
-	
+
 	// on hold
 	@Then("^see the result$")
 	public void see_the_result() throws Throwable 
@@ -175,82 +213,146 @@ public class ManageVatRegistration
 		System.out.println("Result Changed  ");
 	}
 	
+//Pagination 
 	@And("^user click on the page \"([^\"]*)\" and check page\"([^\"]*)\"$")
 	public void user_click_on_the_page_and_check_page(String value, String value2) throws Throwable 
 	{
-
     	try {
             obj.load(fis);
         } catch (IOException e) {
             e.printStackTrace();
         }
 		WebElement we,we1;
+		
 		we=wd.findElement(By.xpath(obj.getProperty(value)));
-		we1=wd.findElement(By.xpath(obj.getProperty(value2)));
-		if(we.isEnabled() && we1.isEnabled())
+		we.click();
+				
+		Thread.sleep(2000);				
+		we1=wd.findElement(By.xpath(obj.getProperty(value2)));		
+		Thread.sleep(2000);
+		
+		if(we.isEnabled() && !we1.isEnabled())
 		{
+			
 			System.out.println("Fail");
 		}
 		else
 		{
 			System.out.println("Pass");
 		}
-	}
-	
+		Thread.sleep(3000);
+	}	
+	//Pagination	
 	@Then("^User see Result$")
 	public void user_see_Result() throws Throwable 
 	{
-		
-			System.out.println("Result Display");
+		System.out.println("Result Display");		
+	}		
+	
+	//For Number Of Row
+	int ColmnCount=wd.findElements(By.xpath("//*[@class='d-flex justify-content-between w-100 p-3 list-header font-medium cursor-pointer']")).size();	
+	@And("^User Click Dropdown \"([^\"]*)\"$")
+	public void user_Type_Dropdown(String value) throws Throwable 
+	{
+		clickOn("TypeList","");
+		if(value.equals("20"))
+		{
+			clickOn("select20","");
+			Thread.sleep(2000);			
+		}
+		else if(value.equals("15"))
+		{
+			clickOn("select15","");
+			Thread.sleep(3000);			
+		}
+		else if(value.equals("25"))
+		{
+			clickOn("select25","");
+			Thread.sleep(3000);
+		}
+		else if(value.equals("50"))
+		{
+			clickOn("select50","");
+			Thread.sleep(2000);
+		}
+		else if(value.equals("100"))
+		{
+			clickOn("select100","");
+			Thread.sleep(2000);
+		}
+		int rowCount=wd.findElements(By.xpath("//*[@class='list-fields d-flex justify-content-between w-100 p-3 font-regular cursor-pointer ng-star-inserted']")).size();
+		System.out.println("rowCount :"+rowCount);
 	}
+	//For Number Of Row
+	@Then("^user see the page result$")
+	public void user_see_the_page_result() throws Throwable 
+	{
+		//int rowCount=wd.findElements(By.xpath("//*[@class='list-fields d-flex justify-content-between w-100 p-3 font-regular cursor-pointer ng-star-inserted']")).size();
+		//System.out.println("rowCount :"+rowCount);
+		System.out.println("ColmnCount :"+ColmnCount);				
+	}	
 	
 	//For Sorting
 	@And("^user click on the sorting \"([^\"]*)\"$")
 	public void user_click_on_the_sorting(String value) throws Throwable 
 	{
 		System.out.println("User Click on Sorting");		
-		
-		clickOn("start","[@class='sorting_" + value + "']");		
+
+		clickOn("start","[@class='pl-1 fa fa-sort-" + value + "']");		
 	}
-	
-	@Then("^User see sorting result$")
-	public void user_see_sorting_result() throws Throwable 
-	{	
-		int rowCount=wd.findElements(By.tagName("tr")).size();
-		int ColmnCount=wd.findElements(By.xpath("//table//thead//tr//th")).size();
+	//For Sorting
+	@Then("^User see sorting result \"([^\"]*)\"$")
+	public void user_see_sorting_result(String value) throws Throwable 
+	{
+		int ColmnCount=wd.findElements(By.xpath("//*[@class='d-flex justify-content-between w-100 p-3 list-header font-medium cursor-pointer']")).size();
+		int rowCount=wd.findElements(By.xpath("//*[@class='list-fields d-flex justify-content-between w-100 p-3 font-regular cursor-pointer ng-star-inserted']")).size();
 		
 		System.out.println("rowCount :"+rowCount);
 		System.out.println("ColmnCount :"+ColmnCount);
+		Thread.sleep(10000);
 		
-			String name= wd.findElement(By.xpath("//*[@id='example']/tbody/tr[1]/td[1]")).getText();
-			System.out.println(name);								
-			
-			if(name.contains("Y. Berry"))
-			{				
-				System.out.println("Desceding order");				
-			}
-			else
-			{
-				System.out.println("No Desceding order");
-			}				 				
+		String name= wd.findElement(By.xpath("(//span[contains(text(),'9')])[1]")).getText();
+		
+		System.out.println(name);		
+		
+		WebElement errorMessage= wd.findElement(By.xpath("(//span[contains(text(),'9')])[1]"));
+		Assert.assertEquals(errorMessage.getText(),value);
 	}
 	
-	//For Show Enteries
-	@And("^user click on Dropdown \"([^\"]*)\"$")
-	public void user_click_on_Dropdown(String value) throws Throwable 
+	//TC_05
+	@When("^user selects filter by and select all$")
+	public void user_selects_filter_by_and_select_all() throws Throwable 
 	{
-		drp_select("selectdropdown", value); 
-		Thread.sleep(1000);
+	   System.out.println("Filter BY All is Selected");
 	}
-
-	/*@Then("^user see the page result$")
-	public void user_see_the_page_result() throws Throwable 
+	//TC_05
+	@Then("^clicks on download pdf button and all record gets downloaded$")
+	public void clicks_on_download_pdf_button_and_all_record_gets_downloaded() throws Throwable 
 	{
-		int rowCount=wd.findElements(By.tagName("tr")).size();
-		int ColmnCount=wd.findElements(By.xpath("//table//thead//tr//th")).size();
+		clickOn("DownloadPDF", "");
+		Thread.sleep(1000);
+		clickOn("DownloadPDFClick", "");		
+		verifyDownload("ManageVatRegistration");
+	}
+	//@FilterCombinationInvalid	
+	@Then("^user checks its result \"([^\"]*)\"$")
+	public void user_checks_its_result(String value) throws Throwable 
+	{	
+		Thread.sleep(3000);
+		 try
+		 {
+	            obj.load(fis);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+		//WebElement errorMessage= wd.findElement(By.xpath("//*[contains(text(),'No records found!')]"));
+		//Assert.assertEquals(errorMessage.getText(),value);
 		
-		System.out.println("rowCount :"+rowCount);
-		System.out.println("ColmnCount :"+ColmnCount);			
-	}*/
+		if(elementText("NoRecord").equalsIgnoreCase("No records"))
+		{
+			System.out.println("Pass");
+		}
+		
+	}
 		
 }
--- src/test/resources/features/ManageVatRegistration.feature
+++ src/test/resources/features/ManageVatRegistration.feature
@@ -1,138 +1,67 @@
 Feature: User is on Manage VAT Registration 
 
-Scenario Outline: Validate Filter Functionality
+#TC_02,TC_03 Name of business, Taxpayer sub Category,Location,Last Updated Date, Tax officer, Status are all common 
+#TC_04,
+#@FilterByDropdown
+Scenario Outline: Validate Filter Functionality for Dropdown
 Given User is on Internal Portal
 When user selects Manage Vat Registraion
-Then user selects value from filter drop down "<Filter>"
+Then user selects value from filter drop down "<Filters>"
 And enters data in type here text box "<Value>"
-Then clicks on download pdf button
+#Then clicks on download pdf button
 
 Examples:
-|Filters|Value|
-|Priority| 1|
-|Priority| 0|
-|Priority| 2|
-|NIF|1717|
-|NIF|JUIJIBI121213|
-|Name of Business/Applicant|Arun|
-|Name of Business/Applicant|ritesh|
-|Taxpayer Sub Category|Individual|
-|Taxpayer Sub Category|Incorporated Bodies|
-|Location|Banana|
-|Last Updated Date|06/19/2018|
-|Last Updated Date|06/18/2018|
-|Tax Officer| officer16 |
-|Tax Officer| officer12 |
-|Status|Rejected|
-|Status|Review Completed|
-|Status|Review In Progress|
+| TestcaseID                   | Filters                   |Value|
+|Valid_Manage_VAT_Reg_Filter_01| Priority                  |10   |
+|Valid_Manage_VAT_Reg_Filter_02| NIF                       |JUIJIBI121213|
+|Valid_Manage_VAT_Reg_Filter_03| Name of Business/Applicant|Arun|
+|Valid_Manage_VAT_Reg_Filter_04| Taxpayer Sub Category     |Individual|
+|Valid_Manage_VAT_Reg_Filter_05| Location                  |Banana|
+|Valid_Manage_VAT_Reg_Filter_06| Last Updated Date         |06/19/2018|
+|Valid_Manage_VAT_Reg_Filter_07| Tax Officer               |Demo officer|
+|Valid_Manage_VAT_Reg_Filter_09| Status                    |Review Completed|
+|Valid_Manage_VAT_Reg_Filter_10| Status                    |Review In Progress|
 
 
+#TC_04
+#@FilterByAll
+Scenario Outline: Validate Filter Functionality for All
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects All from filter dropdown
+And enters data in type here text box "<Value>"
+
+Examples:
+| TestcaseID                   | Value    |
+|Valid_Manage_VAT_Reg_Filter_All_01| test 222 |
+
+
+#TC_05 excel not implemeted from devlop 
+#@FilterAll
 Scenario: Download PDF
 Given User is on Internal Portal
 When user selects Manage Vat Registraion
-And User selects all in filter
+And user selects filter by and select all  
+Then clicks on download pdf button and all record gets downloaded 
+
+#| TestcaseID                         |         
+#|Valid_Manage_VAT_Reg_Download_PDF_01|
+
+
+#TC_06
+Scenario Outline: Validate Admin/Supervisor is able to filter data and then download the pdf/excel file
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
 Then clicks on download pdf button
 
-
-
-Scenario Outline: Validate View Functionality when user select view for Rejected and Review Completed
-
-Given User is on Internal Portal
-When user selects Manage Vat Registraion
-Then user selects value from filter drop down "<Filter>"
-And enters data in type here text box "<Value>"
-And user click on view 
-Then it Navigate to Tax Payer Details
-
-
 Examples:
-|Filters|Value|
-|Status|Rejected|
-|Status|Review Completed|
+|TestcaseID                | Filters    |Value|
+|Valid_Manage_VAT_Reg_DonPDF_01| Priority      |11   |
 
 
-
-Scenario Outline: Validate Pop up when user Click on Rejected 
-
-Given User is on Internal Portal
-When user selects Manage Vat Registraion
-Then user selects value from filter drop down "<Filter>"
-And enters data in type here text box "<Value>"
-And User click on Rejected
-Then User See pop up Reason for Rejected
-
-
-Examples:
-|Filters|Value|
-|Status|Rejected|
-
-
-Scenario Outline: Validate Review in Progress from Filter status and user select Review  
-
-Given User is on Internal Portal
-When user selects Manage Vat Registraion
-Then user selects value from filter drop down "<Filter>"
-And enters data in type here text box "<Value>"
-And user click on view 
-Then it Navigate to Tax Payer Details
-
-
-Examples:
-|Filters|Value|
-|Status|Review in Progress|
-
-
-Scenario Outline: Validate Review in Progress from Filter status and user select Change Priority  
-
-Given User is on Internal Portal
-When user selects Manage Vat Registraion
-Then user selects value from filter drop down "<Filter>"
-And enters data in type here text box "<Value>"
-And user click on Change Priority 
-And select priority from priority list 
-And click on right tick
-Then Priority will change    
-
-
-
-Examples:
-|Filters|Value|
-|Status|Review in Progress|
-
-
-Scenario Outline: Validate Review in Progress from Filter status and user select Reassign  
-
-Given User is on Internal Portal
-When user selects Manage Vat Registraion
-Then user selects value from filter drop down "<Filter>"
-And enters data in type here text box "<Value>"
-And user click on Reassign
-And select another Tax Officier fron the list
-And click on right tick
-Then Tax Officier Changed and toast message display 
-
-
-Examples:
-|Filters|Value|
-|Status|Review in Progress|
-
-#on hold
-Scenario Outline: Validate User is able to change maximum page size
-
-Given User is on Internal Portal
-When user selects Manage Vat Registraion
-And  user select show "<Entries>"
-Then see the result
-
-Examples:
-|Entries|
-|20|
-|25|
-|50|
-|100|
-
-
+#TC_07
 Scenario Outline: User Navigate to particular page
 
 Given User is on Internal Portal
@@ -142,34 +71,144 @@ Then User see Result
 
 Examples:
 
-|ClickPage | CheckPage |
-|Last      | Next      |
-|First     | Previous  |
-|Previous  | First     |
-|Previous  | Next      |
+| TestcaseID                     |ClickPage | CheckPage |
+|Valid_Manage_VAT_Reg_Navigate_01|Last      | Next      |
+|Valid_Manage_VAT_Reg_Navigate_02|Last      | Previous  |
+|Valid_Manage_VAT_Reg_Navigate_03|Next      |Previous   |
+|Valid_Manage_VAT_Reg_Navigate_04|Next      | First     |
+|Valid_Manage_VAT_Reg_Navigate_05|First1    |Previous   |
+|Valid_Manage_VAT_Reg_Navigate_06|First1    |First      |
+
+#TC_08
+#@ShowEnteries
+Scenario Outline: Validate User Click on Show Enteries 
+Given User is on Internal Portal
+And User Click Dropdown "<Value>"
+Then user see the page result 
+
+Examples:
+| TestcaseID                 |Value|
+|Valid_Manage_VAT_Reg_Show_01|20|
+|Valid_Manage_VAT_Reg_show_02|15|
+|Valid_Manage_VAT_Reg_Show_03|25|
+|Valid_Manage_VAT_Reg_Show_04|50|
+|Valid_Manage_VAT_Reg_Show_05|100|
+
+#TC_09
+#@StatusChangePriority
+Scenario Outline: Validate Review in Progress from Filter status and user select Change Priority  
+
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
+And user click on Change Priority 
+And user selects priority from priority list "<ChangePriority>" 
+And click on right tick 
+Then Priority is Changed and toast message display
+
+Examples:
+| TestcaseID                           |Filters|Value             |ChangePriority|
+|Valid_Manage_VAT_Reg_ChangePriority_01|Status |Review in Progress|Ten           |
+
+#TC_10
+#@StatusReassign
+Scenario Outline: Validate Review in Progress from Filter status and user select Reassign  
+
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
+And user click on Reassign
+And select another Tax Officier fron the list "<ChangeOfficer>" 
+And click on right tick for reassign
+Then Tax Officier Changed and toast message display 
+
+Examples:
+| TestcaseID                     |Filters|Value|ChangeOfficer|
+|Valid_Manage_VAT_Reg_Reassign_01|Status|Review in Progress|SelectTaxOfficerOne|
 
 
+#Review complete click than loader comes 
+#@StatusReview
+Scenario Outline: Validate View Functionality when user select view for Rejected and Review Completed
 
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
+And user click on view 
+Then it Navigate to Tax Payer Details
+
+Examples:
+|Filters|Value|
+|Status|Rejected|
+|Status|Review Completed|
+
+
+#TC_11
+#@RejectedPopup
+Scenario Outline: Validate Pop up when user Click on Rejected 
+
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
+And User click on Rejected
+Then User See pop up Reason for Rejected
+
+Examples:
+| TestcaseID                     |Filters|Value|
+|Valid_Manage_VAT_Reg_RejPopUp_01|Status |Rejected|
+
+#TC_13
+#@StatusReviewInProgress
+Scenario Outline: Validate Review in Progress from Filter status and user select Review  
+
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
+And user click on manage setting and select Review 
+Then it Navigate to Tax Payer Details
+
+
+Examples:
+| TestcaseID                    |Filters|Value|
+|Valid_Manage_VAT_Reg_Review_01 |Status|Review in Progress|
+
+#TC_14
 Scenario Outline: Validate User is able to do sorting 
 
 Given User is on Internal Portal
 When user selects Manage Vat Registraion
-And user click on the sorting "<Sorting>"
+And user click on the sorting "<sort>"
+Then User see sorting result "<value>"
 
 Examples:
 
-|Sorting|
-|FixSort|
+| TestcaseID                 |sort|value|
+|Valid_Manage_VAT_Reg_Sort_01|desc|9|
+#|Valid_Manage_VAT_Reg_Sort_02|desc|8|
 
-
-Scenario Outline: Validate User Click on Show Enteries 
-
-Given user is in current page
-And user click on Dropdown "<dropdown>"
-Then user see the page result 
+#TC_16
+#@FilterCombinationInvalid
+Scenario Outline: Validate filter fucntionality and Type here with different combination Invalid 
+Given User is on Internal Portal
+When user selects Manage Vat Registraion
+Then user selects value from filter drop down "<Filters>"
+And enters data in type here text box "<Value>"
+Then user checks its result "<check>"
 
 Examples:
+| TestcaseID                        |Filters                   |Value        |check|
+|Valid_Manage_VAT_Reg_Combination_01|Priority                  |12          |No records found!|
+|Valid_Manage_VAT_Reg_Combination_02|NIF                       |Arun         |No records found!|
+|Valid_Manage_VAT_Reg_Combination_03|Name of Business/Applicant|Individual   |No records found!|
+|Valid_Manage_VAT_Reg_Combination_04|Taxpayer Sub Category     |Banana       |No records found!|
+|Valid_Manage_VAT_Reg_Combination_05|Location                  |06/19/2018   |No records found!|
+|Valid_Manage_VAT_Reg_Combination_06|Last Updated Date         |officer      |No records found!|
+|Valid_Manage_VAT_Reg_Combination_07|Tax Officer               |Rejected     |No records found!|
+|Valid_Manage_VAT_Reg_Combination_08|Status                    |JUIJIBI121213|No records found!|
 
-|dropdown|
-|100|
 
